5d8e596929bcce02dc2e9c46bca703e92bed6730
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-08-22 17:36:37 +0100

    Improve ./test.sh and ./deploy.sh scripts

--- test.sh
+++ test.sh
@@ -12,5 +12,5 @@ cd $TESTS_PATH
 GIT_SSH_COMMAND="ssh -i ~/.ssh/monster_rsa" bundle install
 
 # run cucumber tests against deployed domain
-SDK_SURGE_URL="$DEPLOY_DOMAIN?async=false"
+SDK_SURGE_URL="${DEPLOY_DOMAIN}?async=false"
 bundle exec cucumber TARGET=development USE_SECRETS=false DEBUG=false SDK_SURGE_URL=$SDK_SURGE_URL
