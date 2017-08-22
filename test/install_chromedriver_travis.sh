a4cdff29357674e54389fc7e2b4332bfe7142038
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-08-22 16:46:19 +0100

    Update install_chromedriver_travis.sh script

---
+++ test/install_chromedriver_travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+CHROME_VERSION="google-chrome-beta"
+CHROME_DRIVER_VERSION=2.31
+
+sudo wget --no-verbose -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update -qqy
+sudo apt-get -qqy install ${CHROME_VERSION}
+rm /etc/apt/sources.list.d/google-chrome.list
+rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+
+sudo wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip /tmp/chromedriver_linux64.zip -d /opt
+chmod 755 /opt/chromedriver
+sudo ln -fs /opt/chromedriver /usr/bin/chromedriver
