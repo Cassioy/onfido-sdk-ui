e71037606ea2449a0d9f76d428bab38c995eb5c3
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-08-22 16:49:34 +0100

    More changes to the script

--- test/install_chromedriver_travis.sh
+++ test/install_chromedriver_travis.sh
@@ -9,10 +9,10 @@ sudo wget --no-verbose -q -O - https://dl-ssl.google.com/linux/linux_signing_key
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update -qqy
 sudo apt-get -qqy install ${CHROME_VERSION}
-rm /etc/apt/sources.list.d/google-chrome.list
-rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+sudo rm /etc/apt/sources.list.d/google-chrome.list
+sudo rm -rf /var/lib/apt/lists/* /var/cache/apt/*
 
 sudo wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip /tmp/chromedriver_linux64.zip -d /opt
+sudo unzip /tmp/chromedriver_linux64.zip -d /opt
 chmod 755 /opt/chromedriver
 sudo ln -fs /opt/chromedriver /usr/bin/chromedriver
